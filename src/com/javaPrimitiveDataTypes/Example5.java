985278e638cde85841d30be46a5404e0b9b4eb64
Your Name <you@example.com>
2024-06-25 11:22:46 +0530

    Initial commit

---
+++ src/com/javaPrimitiveDataTypes/Example5.java
@@ -0,0 +1,30 @@
+package com.javaPrimitiveDataTypes;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		
+int value = 45;
+		
+		
+		
+		//non-prmitive data types: arrays and strings
+		int values[] = {45, 67, 89, 90};
+		
+		char name = 'K';
+		
+		char myName[] = {'K', 'I', 'R', 'A', 'N'};
+		
+		String friendName = "Kiran";
+		
+		System.out.println(myName);
+		System.out.println(friendName.charAt(0));
+		
+		
+		
+		
+		
+
+	}
+
+}
